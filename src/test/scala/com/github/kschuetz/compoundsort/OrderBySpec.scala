3791abc6f97dfc7c02f6e83ef2b497b688cbb976
Kevin Schuetz <schuetzk@gmail.com>
2014-02-05 23:55:29 -0600

    comments on testing for idempotency

--- src/test/scala/com/github/kschuetz/compoundsort/OrderBySpec.scala
+++ src/test/scala/com/github/kschuetz/compoundsort/OrderBySpec.scala
@@ -17,9 +17,6 @@ object OrderBySpec extends Properties("OrderBy") {
     baz: Boolean,
     qux: String,
     quux: Option[String],
-    corge: Double,
-    grault: Date,
-    garply: Either[String, Int],
     waldo: (Int, Int),
     fred: Seq[Int]
   )
@@ -52,19 +49,16 @@ object OrderBySpec extends Properties("OrderBy") {
     baz <- arbitrary[Boolean]
     qux <- arbitrary[String]
     quux <- arbitrary[Option[String]]
-    corge <- arbitrary[Double]
-    grault <- arbitrary[Date]
-    garply <- arbitrary[Either[String, Int]]
     waldo <- arbitrary[(Int, Int)]
     fred <- arbitrary[Seq[Int]]
   } yield Thing(
-      foo, bar, baz, qux, quux, corge, grault, garply, waldo, fred
+      foo, bar, baz, qux, quux, waldo, fred
     )
 
   implicit lazy val arbThing: Arbitrary[Thing] = Arbitrary(generateThing)
 
   lazy val generateThings: Gen[Seq[Thing]] = {
-    Gen.choose(5, 100) flatMap { sz => Gen.listOfN(sz, generateThing) }
+    Gen.choose(5, 25) flatMap { sz => Gen.listOfN(sz, generateThing) }
   }
 
   implicit lazy val arbThings: Arbitrary[Seq[Thing]] = Arbitrary(generateThings)
@@ -75,9 +69,10 @@ object OrderBySpec extends Properties("OrderBy") {
     }
 
   val sortOrder2 = orderByFeature[Thing, Boolean](_.baz)(ascending) {
-    orderByFeature[Thing, Long](x => x.waldo._1 & x.waldo._2)(descending)
+    orderByFeature[Thing, Int](_.waldo._1)(descending) {
+      orderByFeature[Thing, Int](_.waldo._2)(ascending)
+    }
   }
-
 
   def isAscending[A](coll: Seq[A])(implicit ord: Ordering[A]) = {
     coll.isEmpty || (coll, coll.tail).zipped.forall(ord.compare(_,_) <= 0)
@@ -110,7 +105,14 @@ object OrderBySpec extends Properties("OrderBy") {
   def groups[A, K](coll: Seq[A])(f: (A) => K): Seq[Seq[A]] =
     coll.groupBy(f).toList.map(_._2)
 
-  property("sortOrder1") = forAll { things: Seq[Thing] =>
+  def isIdempotent[A](coll: Seq[A])(order: (A, A) => Boolean): Boolean = {
+    val sort1 = coll.sortWith(order)
+    val sort2 = sort1.sortWith(order)
+    sort2.corresponds(sort1)(_ == _)
+  }
+
+
+  property("sortOrder1 correct") = forAll { things: Seq[Thing] =>
     val sorted = things.sortWith(sortOrder1)
     isAscending(sorted.map(_.foo)) && {
       groups(sorted)(_.foo).forall { xs =>
@@ -119,12 +121,30 @@ object OrderBySpec extends Properties("OrderBy") {
     }
   }
 
-  property("sortOrder2") = forAll { things: Seq[Thing] =>
+  property("sortOrder2 correct") = forAll { things: Seq[Thing] =>
     val sorted = things.sortWith(sortOrder2)
     isAscending(sorted.map(_.baz)) && {
       groups(sorted)(_.baz).forall { xs =>
-        isDescending(xs.map(x => x.waldo._1 & x.waldo._2))
+        isDescending(xs.map(_.waldo._1)) && {
+          groups(sorted)(_.waldo._2).forall { xs =>
+            isAscending(xs.map(_.waldo._2))
+          }
+        }
       }
     }
   }
+
+  /*
+    In order for a sort to be idempotent, all fields must be specified
+
+  property("sortOrder1 idempotent") = forAll { things: Seq[Thing] =>
+    isIdempotent(things)(sortOrder1)
+  }
+
+  property("sortOrder2 idempotent") = forAll { things: Seq[Thing] =>
+    isIdempotent(things)(sortOrder2)
+  }
+
+   */
+
 }
